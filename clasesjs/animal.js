35023b27d5d6502548599e09a39b5c0b1bf9a3a5
jepepper <juansecaso@gmail.com>
2022-01-18 17:21:10 -0430

    primero

--- clasesjs/animal.js
+++ clasesjs/animal.js
@@ -7,10 +7,10 @@
     }
 
     seLaVie(){
-        console.log(`${this.name} ${this.vive}, ${this.muere}, ${this.seReproduce}`);
+        console.log(`${this.name} ${this.vive},${this.seReproduce}, ${this.muere}`);
     }
 }
 
-let amoeba = new Animal('Yo la amoeba','vivo en todos lados ','muero cuando tu sistema me ataca',' me divido y me reproduzco');
+let amoeba = new Animal('Yo la amoeba','vivo en todos lados ',' me divido y me reproduzco','muero cuando tu sistema me ataca',);
 
 amoeba.seLaVie();
